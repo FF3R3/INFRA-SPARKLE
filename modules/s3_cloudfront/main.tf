c2e49b69e3daff1270f79e7055b39813f45f86f3
FF3R <fran.feresini@gmail.com>
2025-09-22 11:43:52 -0300

    cambios

--- modules/s3_cloudfront/main.tf
+++ modules/s3_cloudfront/main.tf
@@ -1,8 +1,7 @@
 # Bucket para el frontend
 resource "aws_s3_bucket" "frontend" {
   bucket = var.bucket_name
-
-  tags = merge(var.tags, { Name = var.bucket_name })
+  tags   = merge(var.tags, { Name = var.bucket_name })
 }
 
 # Configuración de sitio web estático
@@ -16,20 +15,4 @@ resource "aws_s3_bucket_website_configuration" "frontend" {
   error_document {
     key = "index.html"
   }
-}
-
-# Política pública para leer objetos
-resource "aws_s3_bucket_policy" "frontend" {
-  bucket = aws_s3_bucket.frontend.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.frontend.arn}/*"
-      }
-    ]
-  })
 }
